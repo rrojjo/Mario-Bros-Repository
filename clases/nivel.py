a25c895489d800419c845762f12fdfbc72b1caac
Pablo Rojo <100583910@alumnos.uc3m.es>
2025-11-26 14:32:22 +0100

    feature: Intento de crear el mapa (salió mal)

--- clases/nivel.py
+++ clases/nivel.py
@@ -1,2 +1,18 @@
-# Created by Pablo Rojo Castaño in nov 2025 
-# Universidad Carlos III de Madrid
+# Contiene el nivel de dificultad del juego y la configuración del mismo.
+
+# nivel.py
+class Nivel:
+    def __init__(self, dificultad):
+        self.dificultad = dificultad
+        self.num_cintas = self._configurar_cintas()
+
+    def _configurar_cintas(self):
+        # Basado en la tabla de dificultad del PDF
+        if self.dificultad == "FACIL" or self.dificultad == "CRAZY":
+            return 6  # Cintas 0 a 5
+        elif self.dificultad == "MEDIO":
+            return 8  # Cintas 0 a 7
+        elif self.dificultad == "EXTREMO":
+            return 10  # Cintas 0 a 9 (Total 10 cintas + suelo base)
+        else:
+            return 6
